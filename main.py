133e955cdf7a62150ca32bc73e22c3a893d02d45
DUO <166278499+LiEnLang@users.noreply.github.com>
2024-04-09 15:42:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 The codes are from DUO's work
 Pixel_Sparse_AE_attack is the codes of the pixel sparse AE attack method proposed by our previous work for the video retrieval system,
-Group_Sparse_AE_attack is the codes of the new group sparse  attack method proposed by our current work to improve the query efficiency
+Group_Sparse_AE_attack is the codes of the new group sparse AE attack method proposed by our current work to improve the query efficiency
 """
 
 import json
